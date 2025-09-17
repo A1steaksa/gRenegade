21d06f36b5199720cd831bd80cc7175db9eb8c21
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2025-09-17 14:12:07 -0500

    Reformatted GlobalSettingsClass

--- lua/renhud/client/code/combat/global-settings.lua
+++ lua/renhud/client/code/combat/global-settings.lua
@@ -3,39 +3,52 @@
 --- @class Renegade
 local CNC = CNC_RENEGADE
 
-local STATIC
+--- @class GlobalSettingsClass
+local STATIC = CNC.CreateExport()
+local CLASS = "GlobalSettings"
 
---[[ Class Setup ]] do
+STATIC.Colors = {}
 
-    --- The static components of GameType
-    --- @class GlobalSettings
-    STATIC = CNC.CreateExport()
+--[[ Teams ]] do
+
+    -- Original teams
+    STATIC.Colors.Nod                = Color( 255,   0,   0 )
+    STATIC.Colors.GDI                = Color( 255, 255,   0 )
+    STATIC.Colors.Neutral            = Color( 255, 255, 255 )
+    STATIC.Colors.Mutant             = Color(   0, 255,   0 )
+    STATIC.Colors.Renegade           = Color(   0,   0, 255 )
+
+    -- New teams
+    STATIC.Colors.Combine            = Color( 198, 131,  10 )
+    STATIC.Colors.Rebels             = Color( 251, 127,  12 )
+    STATIC.Colors.BlackMesa          = Color( 251, 127,  12 )
+    STATIC.Colors.HECU               = Color(  15,   0, 231 )
+    STATIC.Colors.Aperture           = Color(   0, 153, 254 )
+end
+
+--[[ Objectives ]] do
+
+    STATIC.Colors.PrimaryObjective   = Color(   0, 255,   0 )
+    STATIC.Colors.SecondaryObjective = Color(   0,   0, 255 )
+    STATIC.Colors.TertiaryObjective  = Color( 255,   0, 255 )
 end
 
 
---[[ Static Functions and Variables ]] do
+--[[ Health UI ]] do
 
-    local CLASS = "GlobalSettings"
+    STATIC.Colors.HealthHigh         = Color(   0, 255,   0 )
+    STATIC.Colors.HealthMed          = Color( 255, 255,   0 )
+    STATIC.Colors.HealthLow          = Color( 255,   0,   0 )
+end
 
-    --- [[ Public ]]
+--[[ Relationship ]] do
 
-    --- @class GlobalSettings
-    --- @field Colors table<string, Color>
-
-    STATIC.Colors = {}
-    STATIC.Colors.Nod                = Color( 255,   0, 0   )
-    STATIC.Colors.GDI                = Color( 255, 255, 0   )
-    STATIC.Colors.Neutral            = Color( 255, 255, 255 )
-    STATIC.Colors.Mutant             = Color(   0, 255, 0   )
-    STATIC.Colors.Renegade           = Color(   0,   0, 255 )
-    STATIC.Colors.PrimaryObjective   = Color(   0, 255, 0   )
-    STATIC.Colors.SecondaryObjective = Color(   0,   0, 255 )
-    STATIC.Colors.TertiaryObjective  = Color( 255,   0, 255 )
-    STATIC.Colors.HealthHigh         = Color(   0, 255, 0   )
-    STATIC.Colors.HealthMed          = Color( 255, 255, 0   )
-    STATIC.Colors.HealthLow          = Color( 255,   0, 0   )
-    STATIC.Colors.Enemy              = Color( 255,   0, 0   )
-    STATIC.Colors.Friendly           = Color(   0, 255, 0   )
+    STATIC.Colors.Enemy              = Color( 255,   0,   0 )
+    STATIC.Colors.Friendly           = Color(   0, 255,   0 )
     STATIC.Colors.NoRelation         = Color( 255, 255, 255 )
-    STATIC.Colors.ReticleBusy        = Color( 255, 255, 0   )
+end
+
+--[[ Reticle ]] do
+
+    STATIC.Colors.ReticleBusy        = Color( 255, 255,   0 )
 end
