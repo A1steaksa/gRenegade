bf9bcc7aaac47230b5909b28655ae61693becef9
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2025-10-17 09:49:41 -0500

    Move atan2 to WWMath

--- lua/renhud/client/code/wwmath/wwmath.lua
+++ lua/renhud/client/code/wwmath/wwmath.lua
@@ -5,26 +5,24 @@ local CNC = CNC_RENEGADE
 
 --- The static components of WWMath
 --- @class WWMathClass
-local STATIC = CNC.CreateExport()
+local LIB = CNC.CreateExport()
 local CLASS = "WWMath"
-local isHotload = not table.IsEmpty( STATIC )
+local isHotload = not table.IsEmpty( LIB )
 
 
-STATIC.EPSILON  = 0.0001
-STATIC.EPSILON2 = STATIC.EPSILON * STATIC.EPSILON
-STATIC.PI       = 3.141592654
-STATIC.SQRT2    = 1.414213562
-STATIC.SQRT3    = 1.732050808
-STATIC.OOSQRT2  = 0.707106781
-STATIC.OOSQRT3  = 0.577350269
+LIB.EPSILON  = 0.0001
+LIB.EPSILON2 = LIB.EPSILON * LIB.EPSILON
+LIB.PI       = 3.141592654
+LIB.SQRT2    = 1.414213562
+LIB.SQRT3    = 1.732050808
+LIB.OOSQRT2  = 0.707106781
+LIB.OOSQRT3  = 0.577350269
 
-
---- Implemented as poorly as it was in the original code
 --- @param val number
 --- @param min number
 --- @param max number
 --- @return number
-function STATIC.Wrap( val, min, max )
+function LIB.Wrap( val, min, max )
     -- "Implemented as an if rather than a while, to long loops"
 
     if val >= max then val = val - (max-min) end
@@ -38,6 +36,21 @@ end
 
 --- "Inverse square root"
 --- @param val number
-function STATIC.InvSqrt( val )
+function LIB.InvSqrt( val )
     return 1.0 / math.sqrt( val )
+end
+
+--- @param y number
+--- @param x number
+--- @return number
+function LIB.Atan2( y, x )
+    -- Convert -0 to 0
+    if y == -y then y = 0 end
+    if X == -x then X = 0 end
+
+    -- Very small non-zero values might be causing problems 
+    if math.IsNearlyEqual( y, 0, 0.01 ) then y = 0 end
+    if math.IsNearlyEqual( x, 0, 0.01 ) then x = 0 end
+
+    return math.atan2( y, x )
 end
