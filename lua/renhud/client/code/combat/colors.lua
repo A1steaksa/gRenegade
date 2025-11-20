af1ec41305410503586f2ba8a9ac7319102ca565
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2025-11-20 11:29:01 -0600

    Add color class

---
+++ lua/renhud/client/code/combat/colors.lua
@@ -0,0 +1,46 @@
+-- Based on colors within Code/Combat/colors.cpp/h
+
+--- @class Renegade
+local CNC = CNC_RENEGADE
+
+--- @class ColorClass
+local STATIC = CNC.CreateExport()
+local isHotload = not table.IsEmpty( STATIC )
+
+--#region Imports
+
+    --- @type PlayerType
+    local playerType = CNC.Import( "renhud/code/combat/player-type.lua" )
+--#endregion
+
+
+--#region Imported Enums
+
+    local playerTypeEnum = playerType.PLAYER_TYPE_ENUM
+--#endregion
+
+
+--- @class ColorClass
+--- @field TeamColors table<PlayerTypeEnum, Color>
+-- --- @field TextColors table<integer, Color>
+
+STATIC.TeamColors = {
+    [playerTypeEnum.Nod]        = Color( 255,   0,   0 ),
+    [playerTypeEnum.GDI]        = Color( 255, 204,   0 ),
+    [playerTypeEnum.Neutral]    = Color( 204, 204, 204 ),
+    [playerTypeEnum.Renegade]   = Color( 255, 255, 255 )
+}
+
+-- STATIC.TextColors = {
+--     COLOR_PUBLIC_TEXT    = Vector3( 1.0f, 1.0f, 1.0f );
+--     COLOR_PRIVATE_TEXT	= Vector3( 0.0f, 0.0f, 1.0f );
+--     COLOR_PAGED_TEXT     = Vector3( 0.0f, 1.0f, 1.0f );
+--     COLOR_INVITE_TEXT    = Vector3( 1.0f, 0.0f, 1.0f );
+--     COLOR_CONSOLE_TEXT	= Vector3( 1.0f, 1.0f, 1.0f );
+-- }
+
+--- @param team PlayerTypeEnum
+function STATIC.GetColorForTeam( team )
+    --- TODO: Tie this into a team API of some kind 
+    return STATIC.TeamColors[ team ]
+end
