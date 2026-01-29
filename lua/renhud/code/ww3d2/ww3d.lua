d291b442561008d3c732a80b3a9973353e3048db
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2026-01-29 11:02:36 -0600

    Add a bunch of stubbed functions

--- lua/renhud/code/ww3d2/ww3d.lua
+++ lua/renhud/code/ww3d2/ww3d.lua
@@ -8,6 +8,10 @@ local STATIC = CNC.CreateExport()
 STATIC.Class = "WW3dClass"
 local isHotload = not table.IsEmpty( STATIC )
 
+--- @type WW3dClass
+
+STATIC.DefaultNativeScreenSize = 1.0
+STATIC._IsScreenUvBiased = true
 
 --- @param isBiased boolean `true` if 2D rendering should be biased, `false` otherwise
 function STATIC.SetScreenUvBias( isBiased )
@@ -16,5 +20,54 @@ end
 
 --- @return boolean `true` if 2D rendering should be biased, `false` otherwise
 function STATIC.IsScreenUvBiased()
-    return STATIC._IsScreenUvBiased
+    return STATIC._IsScreenUvBiased and true or false
+end
+
+--[[ Texture Reduction ]] do
+
+    function STATIC.GetDefaultNativeScreenSize()
+        return STATIC.DefaultNativeScreenSize
+    end
+end
+
+--[[ Rendering Functions ]] do
+
+    --- "  
+    --- Each frame should be bracketed by a Begin_Render and End_Render call.  Between these two calls you will
+    --- normally render scenes.  The render function which accepts a single render object is implemented for
+    --- special cases like generating a shadow texture for an object.  Basically this function will have the
+    --- entire scene rendering overhead.  
+    --- "  
+
+    --- @param
+    --- @return 
+    function STATIC.BeginRender()
+        typecheck.NotImplementedError()
+    end
+
+    --- @param
+    --- @return 
+    function STATIC.Render()
+        typecheck.NotImplementedError()
+    end
+
+    --- @param
+    --- @return 
+    function STATIC.Flush()
+        typecheck.NotImplementedError()
+    end
+
+    --- @param
+    --- @return 
+    function STATIC.EndRender()
+        typecheck.NotImplementedError()
+    end
+
+    --- @param
+    --- @return 
+    function STATIC.FlipToPrimary()
+        typecheck.NotImplementedError()
+    end
+
+
 end
