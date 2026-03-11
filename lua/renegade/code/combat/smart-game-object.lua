8f85e46b6fabe4f8024468c54afde0df43a6791e
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2026-03-11 12:18:07 -0500

    Add chunk IDs

--- lua/renegade/code/combat/smart-game-object.lua
+++ lua/renegade/code/combat/smart-game-object.lua
@@ -4,11 +4,11 @@
 local CNC = CNC_RENEGADE
 
 --- @type ArmedGameObjectClass
-local PARENT = CNC.Import( "code/combat/armed-game-object.lua" )
+local armedGameObjectClass = CNC.Import( "code/combat/armed-game-object.lua" )
 
 --- @class SmartGameObjectClass : ArmedGameObjectClass
 --- @field Instance SmartGameObjectInstance The metatable used by SmartGameObjectInstance
-local STATIC = CNC.CreateExport( PARENT )
+local STATIC = CNC.CreateExport( armedGameObjectClass )
 local isHotload = not table.IsEmpty( STATIC )
 STATIC.Class = "SmartGameObjectClass"
 --- @class SmartGameObjectInstance : ArmedGameObjectInstance
@@ -19,6 +19,9 @@ STATIC.Instance = INSTANCE
 INSTANCE.Static = STATIC
 INSTANCE.IsSmartGameObject = true
 
+--#region Exported Enums
+
+--#endregion
 
 
 --#region Imports
@@ -30,16 +33,42 @@ INSTANCE.IsSmartGameObject = true
 --#region Imported Enums
 --#endregion
 
+
 --[[ Chunk IDs ]] do
 
     local enumBuilder = enumBuilderClass.New()
 
     STATIC.ChunkIds = {
-        CHUNKID_PLACEHOLDER = enumBuilder:Set( 0 ),
-        CHUNKID_PLACEHOLDER = enumBuilder:Next(),
+        OLD_CHUNKID_PHYSICALGAMEOBJ_PARENT = enumBuilder:Set( 910991113 ),
+        CHUNKID_VARIABLES                  = enumBuilder:Next(),
+        CHUNKID_CONTROL                    = enumBuilder:Next(),
+        CHUNKID_CONTROLLER                 = enumBuilder:Next(),
+        CHUNKID_ACTION                     = enumBuilder:Next(),
+        XXXCHUNKID_WEAPONBAG               = enumBuilder:Next(),
+        CHUNKID_ARMEDGAMEOBJ_PARENT        = enumBuilder:Next(),
+        XXXCHUNKID_PLAYER_DATA             = enumBuilder:Next(),
+        CHUNKID_STEALTH_EFFECT             = enumBuilder:Next(),
+
+        MICROCHUNKID_CONTROL_ENABLED    = enumBuilder:Set( 1 ),
+
+        XXXMICROCHUNKID_WEAPON_TILT     = enumBuilder:Next(),
+        XXXMICROCHUNKID_WEAPON_TURN     = enumBuilder:Next(),
+        MICROCHUNKID_CONTROL_OWNER      = enumBuilder:Next(),
+        XXX_MICROCHUNKID_IS_GHOST       = enumBuilder:Next(),
+        MICROCHUNKID_IMPORT_STATE_COUNT = enumBuilder:Next(),
+        MICROCHUNKID_TINT_COLOR         = enumBuilder:Next(),
+
+        MICROCHUNKID_CONTROLLER_PTR        = enumBuilder:Next(),
+        MICROCHUNKID_IS_ENEMY_SEEN_ENABLED = enumBuilder:Next(),
+        XXXMICROCHUNKID_TARGETING_POS      = enumBuilder:Next(),
+        MICROCHUNKID_MOVING_SOUND_TIMER    = enumBuilder:Next(),
+        MICROCHUNKID_PLAYER_DATA           = enumBuilder:Next(),
+
+        MICROCHUNKID_STEALTH_ENABLED       = enumBuilder:Next(),
+        MICROCHUNKID_STEALTH_POWERUP_TIMER = enumBuilder:Next(),
+        MICROCHUNKID_STEALTH_FIRING_TIMER  = enumBuilder:Next()
     }
 end
-
 
 
 --[[ Static Functions and Variables ]] do
