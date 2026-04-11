80fd1e3d22dc0964a7e340e3421cd125ce8e0fcb
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2026-04-11 09:35:20 -0500

    Add simple game object and definition classes

---
+++ lua/renegade/code/combat/simple-game-object-definition.lua
@@ -0,0 +1,129 @@
+-- Based on SimpleGameObjDef within Code/Combat/simplegameobj.h
+
+--- @class Renegade
+local CNC = CNC_RENEGADE
+
+--- @type PhysicalGameObjectDefinitionClass
+local physicalGameObjectDefinitionClass = CNC.Import( "code/combat/physical-game-object-definition.lua" )
+
+--- @class SimpleGameObjectDefinitionClass : PhysicalGameObjectDefinitionClass
+--- @field Instance SimpleGameObjectDefinitionInstance The metatable used by SimpleGameObjectDefinitionInstance
+local STATIC = CNC.CreateExport( physicalGameObjectDefinitionClass )
+local isHotload = not table.IsEmpty( STATIC )
+STATIC.Class = "SimpleGameObjectDefinitionClass"
+
+--- @class SimpleGameObjectDefinitionInstance : PhysicalGameObjectDefinitionInstance
+--- @field Static SimpleGameObjectDefinitionClass The static table for this instance's class
+local INSTANCE = robustclass.Register( "Renegade_SimpleGameObjectDefinition" )
+INSTANCE.Class = "SimpleGameObjectDefinitionInstance"
+STATIC.Instance = INSTANCE
+INSTANCE.Static = STATIC
+INSTANCE.IsSimpleGameObjectDefinition = true
+
+--#region Exported Enums
+--#endregion
+
+--#region Imports
+
+    --- @type PlayerTerminalClass
+    local playerTerminalClass = CNC.Import( "code/combat/player-terminal.lua" )
+
+    --- @type SimplePersistFactoryClass
+    local simplePersistFactoryClass = CNC.Import( "code/wwsaveload/simple-persist-factory.lua" )
+
+    --- @type SimpleDefinitionFactoryClass
+    local simpleDefinitionFactoryClass = CNC.Import( "code/wwsaveload/simple-definition-factory.lua" )
+
+    --- @type CombatChunkId
+    local combatChunkId = CNC.Import( "code/combat/combat-chunk-id.lua" )
+
+    --- @type SimpleGameObjectClass
+    local simpleGameObjectClass = CNC.Import( "code/combat/simple-game-object.lua" )
+--#endregion
+
+--#region Imported Enums
+
+    local typeEnum = playerTerminalClass.TYPE
+--#endregion
+
+--[[ Static Functions and Variables ]] do
+
+    --- @class SimpleGameObjectDefinitionClass
+
+    --- Creates a new SimpleGameObjectDefinitionInstance
+    --- @return SimpleGameObjectDefinitionInstance
+    function STATIC.New()
+        return robustclass.New( "Renegade_SimpleGameObjectDefinition" )
+    end
+
+    function STATIC.StaticConstructor()
+        STATIC.SimpleGameObjectDefinitionPersistFactory = simplePersistFactoryClass.New( STATIC, combatChunkId.CHUNKID_GAME_OBJECT_DEF_SIMPLE )
+        STATIC.SimpleGameObjectDefinitionDefinitionFactory = simpleDefinitionFactoryClass.New( STATIC, combatChunkId.CLASSID_GAME_OBJECT_DEF_SIMPLE, "Simple" )
+    end
+
+    --- @param arg any
+    --- @return boolean `true` if the passed argument is a(n) SimpleGameObjectDefinitionInstance, `false` otherwise
+    function STATIC.IsSimpleGameObjectDefinition( arg )
+        if not istable( arg ) then return false end
+        if getmetatable( arg ) ~= INSTANCE then return false end
+
+        return arg.IsSimpleGameObjectDefinition and true or false
+    end
+
+    typecheck.RegisterType( "SimpleGameObjectDefinitionInstance", STATIC.IsSimpleGameObjectDefinition )
+end
+
+
+--- @class SimpleGameObjectDefinitionInstance
+--- @field IsEditorObject boolean
+--- @field IsHiddenObject boolean
+--- @field PlayerTerminalType Type
+
+function INSTANCE:Renegade_SimpleGameObjectDefinition()
+    self.IsEditorObject = false
+    self.IsHiddenObject = false
+    self.PlayerTerminalType = typeEnum.TYPE_NONE
+end
+
+--- @return integer
+function INSTANCE:GetClassId()
+    return combatChunkId.CLASSID_GAME_OBJECT_DEF_SIMPLE
+end
+
+--- @param connectedEntity Entity
+--- @return PersistInstance
+function INSTANCE:Create( connectedEntity )
+    local object = simpleGameObjectClass.New()
+    object:Init( self, connectedEntity )
+    return object
+end
+
+--- @param csave ChunkSaveInstance
+function INSTANCE:Save( csave )
+    typecheck.NotImplementedError()
+end
+
+--- @param cload ChunkLoadInstance
+function INSTANCE:Load( cload )
+    typecheck.NotImplementedError()
+end
+
+--- @return PersistFactoryInstance
+function INSTANCE:GetFactory()
+    return STATIC.SimpleGameObjectDefinitionPersistFactory
+end
+
+
+--[[ Accessors ]] do
+
+    --- @return Type
+    function INSTANCE:GetPlayerTerminalType()
+        return self.PlayerTerminalType
+    end
+
+    --- @return boolean
+    function INSTANCE:GetIsEditorObject()
+        return self.IsEditorObject
+    end
+end
+
---
+++ lua/renegade/code/combat/simple-game-object.lua
@@ -0,0 +1,161 @@
+-- Based on SimpleGameObj within Code/Combat/simplegameobj.h
+
+--- @class Renegade
+local CNC = CNC_RENEGADE
+
+--- @type PhysicalGameObjectClass
+local physicalGameObjectClass = CNC.Import( "code/combat/physical-game-object.lua" )
+
+--- @class SimpleGameObjectClass : PhysicalGameObjectClass
+--- @field Instance SimpleGameObjectInstance The metatable used by SimpleGameObjectInstance
+local STATIC = CNC.CreateExport( physicalGameObjectClass )
+local isHotload = not table.IsEmpty( STATIC )
+STATIC.Class = "SimpleGameObjectClass"
+
+--- @class SimpleGameObjectInstance : PhysicalGameObjectInstance
+--- @field Static SimpleGameObjectClass The static table for this instance's class
+local INSTANCE = robustclass.Register( "Renegade_SimpleGameObject" )
+INSTANCE.Class = "SimpleGameObjectInstance"
+STATIC.Instance = INSTANCE
+INSTANCE.Static = STATIC
+INSTANCE.IsSimpleGameObject = true
+
+--#region Exported Enums
+--#endregion
+
+--#region Imports
+
+    --- @type BaseGameObjectClass
+    local baseGameObjectClass = CNC.Import( "code/combat/base-game-object.lua" )
+
+    --- @type SimplePersistFactoryClass
+    local simplePersistFactoryClass = CNC.Import( "code/wwsaveload/simple-persist-factory.lua" )
+
+    --- @type CombatChunkId
+    local combatChunkId = CNC.Import( "code/combat/combat-chunk-id.lua" )
+--#endregion
+
+--#region Imported Enums
+--#endregion
+
+--[[ Static Functions and Variables ]] do
+
+    --- @class SimpleGameObjectClass
+    --- @field SimpleGameObjectPersistFactory SimplePersistFactoryInstance
+
+    --- Creates a new SimpleGameObjectInstance
+    --- @return SimpleGameObjectInstance
+    function STATIC.New()
+        return robustclass.New( "Renegade_SimpleGameObject" )
+    end
+
+    function STATIC.StaticConstructor()
+        STATIC.SimpleGameObjectPersistFactory = simplePersistFactoryClass.New( STATIC, combatChunkId.CHUNKID_GAME_OBJECT_SIMPLE )
+    end
+
+    --- @param arg any
+    --- @return boolean `true` if the passed argument is a(n) SimpleGameObjectInstance, `false` otherwise
+    function STATIC.IsSimpleGameObject( arg )
+        if not istable( arg ) then return false end
+        if getmetatable( arg ) ~= INSTANCE then return false end
+
+        return arg.IsSimpleGameObject and true or false
+    end
+
+    typecheck.RegisterType( "SimpleGameObjectInstance", STATIC.IsSimpleGameObject )
+end
+
+
+--- @class SimpleGameObjectInstance
+
+function INSTANCE:Renegade_SimpleGameObject()
+    -- self:SetAppPacketType( appPacketTypeEnum.APPPACKETTYPE_SIMPLE )
+end
+
+function INSTANCE:__delete()
+    -- Empty in the original code
+end
+
+
+--[[ Definitions ]] do
+
+    --- @param definition SimpleGameObjectDefinitionInstance?
+    --- @param connectedEntity Entity
+    function INSTANCE:Init( definition, connectedEntity )
+        if not definition then
+            definition = self:GetDefinition()
+        end
+
+        physicalGameObjectClass.Instance.Init( self, definition, connectedEntity )
+    end
+
+    --- @return SimpleGameObjectDefinitionInstance
+    function INSTANCE:GetDefinition()
+        return baseGameObjectClass.Instance:GetDefinition() --[[@as SimpleGameObjectDefinitionInstance]]
+    end
+end
+
+
+--[[ RITTI ]] do
+
+    --- @return SimpleGameObjectInstance
+    function INSTANCE:AsSimpleGameObject()
+        return self
+    end
+end
+
+
+function INSTANCE:IsHiddenObject()
+    return self:GetDefinition().IsHiddenObject
+end
+
+
+--[[ Save/Load/Construction Factory ]] do
+
+    --- @param csave ChunkSaveInstance
+    --- @return boolean
+    function INSTANCE:Save( csave )
+        typecheck.NotImplementedError()
+    end
+
+    --- @param cload ChunkLoadInstance
+    --- @return boolean
+    function INSTANCE:Load( cload )
+        typecheck.NotImplementedError()
+    end
+
+    --- @return PersistFactoryInstance
+    function INSTANCE:GetFactory()
+        return STATIC.SimpleGameObjectPersistFactory
+    end
+
+    function INSTANCE:OnPostLoad()
+        physicalGameObjectClass.Instance.OnPostLoad( self )
+
+        -- "  
+        -- NOTE: the [OnPostLoad] function is only run when loading a level in the engine
+        -- so we can put game-specific behavior into this function without messing up the
+        -- level editor.  
+        -- "  
+        if self:GetDefinition().IsEditorObject then
+            -- "Switch to a NULL model"
+            self:PeekPhysicalObject():SetModelByName( "" )
+            -- "And clear [AnimationControl]"
+            if self:GetAnimationControl() then
+                self:GetAnimationControl():SetModel( self:PeekModel() )
+            end
+        end
+
+        if self:IsHiddenObject() then
+            self:GetConnectedEntity():SetNoDraw( true )
+        end
+    end
+end
+
+
+--[[ Network Support ]] do
+
+    function INSTANCE:IsAlwaysDirty()
+        return false
+    end
+end
